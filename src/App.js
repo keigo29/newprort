79f63315254c44f7c35ef63b573c58e59b89df93
ishikura keigo <21im0015@i-u.ac.jp>
2023-05-26 11:01:11 +0900

    First commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,37 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <h1 className='head-logo'>Portfolio 2023</h1>
+          <ul className='headul'>
+            <li><img src="./mdi_twitter.png" width={20} /></li>
+            <li className='headtxt'>Wantedly</li>
+          </ul>
+        </nav>
+        <div className='mv'>
+
+        <h1 className='mvh1'>
+          KEIGO<br/>
+<span>ISHIKURA</span>
+</h1>
+<p className='mvtxt'>
+Frontend engineer/<br/>
+Ui/Ux designer/<br/>
+web engineer.<br/>
+</p>
+        </div>
+        
+       <div className='mvbtm'>
+        <div>
+        Scroll
+          </div>
+        <img src="/Arrow 1.png" alt="" height={30} />
+       </div>
+       <div className='bottmye'> 
+         　　　
+       </div>
       </header>
+
     </div>
   );
 }
